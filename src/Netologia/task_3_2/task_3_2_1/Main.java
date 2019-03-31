422b32826affa6a833ba60b6cb62d073c382ae8a
Mike <mike-sheff@yandex.ru>
2019-03-31 21:59:38 +0300

    Домашка 3.2

--- src/Netologia/task_3_2/task_3_2_1/Main.java
+++ src/Netologia/task_3_2/task_3_2_1/Main.java
@@ -1,6 +1,7 @@
 package Netologia.task_3_2.task_3_2_1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     final static int SIZE = 8;
@@ -9,6 +10,46 @@ public class Main {
         Random random = new Random();
         int[][] colors = new int[SIZE][SIZE];
         int[][] rotatedColors = new int[SIZE][SIZE];
+        Scanner scanner = new Scanner(System.in);
+        int turn = 0;
+
+        System.out.println("Задача: Поворот матрицы на 90 градусов по часовой стрелке.");
+        System.out.println();
+
+        System.out.println("Выберите следующие варианты поворота:");
+        System.out.println("1 - по часовой ");
+        System.out.println("2 - против часовой");
+
+        while ((turn <= 0) || (turn > 2)) {
+            System.out.print("Ваш вариант: ");
+            try {
+                turn = Integer.parseInt(scanner.next());
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Введите целое число!");
+                turn = 0;
+            }
+        }
+        System.out.println("Выберите следующие варианты поворота:");
+        System.out.println("1 - 90 градусов или 90+360*n, где n - любое целое число");
+        System.out.println("2 - 180 градусов или 180+360*n, где n - любое целое число");
+        System.out.println("3 - 270 градусов или 270+360*n, где n - любое целое число");
+        System.out.println("4 - 360 градусов или 360*n, где n - любое целое число");
+
+        int turnOn = 0;
+
+        while ((turnOn < 1) || (turnOn > 4)) {
+            System.out.print("Ваш вариант: ");
+            try {
+                turnOn = Integer.parseInt(scanner.next());
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Введите целое число!");
+                turnOn = 0;
+            }
+        }
+
+        if (turnOn == 4) {
+            turnOn = 0;
+        }
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -16,21 +57,20 @@ public class Main {
             }
         }
 
+        System.out.println("Матрица до поворота: ");
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 System.out.format("%4d", colors[i][j]);
             }
             System.out.println();
         }
-        int chet = 1;
-        boolean clockise = false;
 
-        for(int k = 0;k < chet; k++) {
+        for (int k = 0; k < turnOn; k++) {
             for (int i = 0; i < SIZE; ++i) {
                 for (int j = 0; j < SIZE; ++j) {
-                    if (clockise) {
+                    if (turn == 1) {
                         rotatedColors[i][j] = colors[SIZE - 1 - j][i];
-                    } else{
+                    } else {
                         rotatedColors[i][j] = colors[j][SIZE - 1 - i];
                     }
                 }
@@ -43,11 +83,15 @@ public class Main {
         }
 
         System.out.println("");
+        System.out.println("Матрица после поворота: ");
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                System.out.format("%4d", rotatedColors[i][j]);
+                System.out.format("%4d", colors[i][j]);
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println("До свидания!");
     }
 }
--- src/Netologia/task_3_2/task_3_2_2/Main.java
+++ src/Netologia/task_3_2/task_3_2_2/Main.java
@@ -1,36 +1,107 @@
 package Netologia.task_3_2.task_3_2_2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    final static int SIZE = 10;
+    final static int SIZE = 5; //10;
     final static int EMTY = 0;
     final static int SHIP = 1;
     final static int DEAD = 2;
     final static int MISS = 3;
     static int warField[][] = new int[SIZE][SIZE];
-    final static int MAX_COUNT = 10;
+    final static int MAX_COUNT = 5; //10;
+    final static int MAX_MOVE = 10; //30;
 
     public static void main(String[] args) {
         Random random = new Random();
+        int deadCount = 0;
+        Scanner scanner = new Scanner(System.in);
+        int shipPlace1 = -1;
+        int shipPlace2 = -1;
+
+        System.out.println("Задача: Морской бой.");
+        System.out.println();
 
         for (int i = 0; i < MAX_COUNT; i++) {
-            int shipPlace1 = random.nextInt(SIZE);
-            int shipPlace2 = random.nextInt(SIZE);
+            shipPlace1 = random.nextInt(SIZE);
+            shipPlace2 = random.nextInt(SIZE);
 
-            if (warField[shipPlace1][shipPlace2] == SHIP){
+            if (warField[shipPlace1][shipPlace2] == SHIP) {
                 i--;
             } else {
                 warField[shipPlace1][shipPlace2] = SHIP;
             }
         }
 
+        int k = 1;
 
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                System.out.format("%3d", warField[i][j]);
+        while (true) {
+            System.out.println("Текущее поле (на шаге № " + k + "): ");
+
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.format("%3d", warField[i][j]);
+                }
+                System.out.println();
             }
-            System.out.println();
+
+            shipPlace1 = -1;
+            shipPlace2 = -1;
+
+            while ((shipPlace1 < 0) || (shipPlace1 >= SIZE)) {
+                System.out.print("Введите координату по горизонтали (y) от 0 до " + (SIZE - 1) + ": ");
+                try {
+                    shipPlace1 = Integer.parseInt(scanner.next());
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Введите целое число!");
+                    shipPlace1 = -1;
+                }
+            }
+            while ((shipPlace2 < 0) || (shipPlace2 >= SIZE)) {
+                System.out.print("Введите координату по вертикали (x) от 0 до " + (SIZE - 1) + ": ");
+                try {
+                    shipPlace2 = Integer.parseInt(scanner.next());
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Введите целое число!");
+                    shipPlace2 = -1;
+                }
+            }
+
+            switch (warField[shipPlace1][shipPlace2]) {
+                case 0:
+                    warField[shipPlace1][shipPlace2] = 3;
+                    System.out.println("Промахнулся!");
+                    break;
+                case 1:
+                    warField[shipPlace1][shipPlace2] = 2;
+                    deadCount++;
+                    System.out.println("Попал!");
+                    break;
+                case 2:
+                    System.out.println("Вы уже сюда стреляли и попали!");
+                    k--;
+                    break;
+                case 3:
+                    System.out.println("Вы уже сюда стреляли и промахнулись!");
+                    k--;
+                    break;
+            }
+
+            if ((deadCount == MAX_COUNT) || (k == MAX_MOVE)) {
+                System.out.format("Совершено шагов - %d \nУбито кораблей - %d \n", k, deadCount);
+                break;
+            }
+            k++;
         }
+
+        if (deadCount == MAX_COUNT) {
+            System.out.print("Вы выйграли!");
+        } else {
+            System.out.println("Вы проиграли!");
+        }
+
+        System.out.println();
+        System.out.println("До свидания!");
     }
 }
