80253a0e24385a8a208293a321e02ca0fd90cef6
Mike <mike-sheff@yandex.ru>
2019-04-15 01:50:37 +0300

    Домашка c дополнительными заданиями. Реализовать.

---
+++ src/Netologia/task_4_2/task_4_2_2_2/Main.java
@@ -0,0 +1,7 @@
+package Netologia.task_4_2.task_4_2_2_2;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO: реализовать нормальную польскую запись
+    }
+}
---
+++ src/Netologia/task_4_4/task_4_4_3/Main.java
@@ -0,0 +1,8 @@
+package Netologia.task_4_4.task_4_4_3;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO: сделать ополнительную задачу
+
+    }
+}
